2d828dbaf73fe6ce7ef19e1ebcfb20d4b8240f9e
yhee <yhee2075@naver.com>
2023-08-12 12:08:51 +0900

    Add:Nav_animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,15 @@
 const moBtn = document.getElementById('moBtn');
+const nav = document.querySelector('nav');
+const header = document.querySelector('header');
 
-moBtn.addEventListener('click', () => {
-  const nav = document.querySelector('nav');
-  nav.classList.toggle('on');
+// 모바일 메뉴 닫기
+document.addEventListener('click', e => {
+  const isOn = nav.classList.contains('on');
+  const isNotMenu = !e.target.classList.contains('notClose');
+  const isMoBtn = e.target.classList.contains('moBtn');
+
+  if (isNotMenu) {
+    if (isMoBtn) nav.classList.toggle('on');
+    else nav.classList.remove('on');
+  }
 });
-
-const list = document.querySelectorAll('nav ul li');
